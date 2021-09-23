7e597e4e524bcc0ae1b31cca35b765a0342c2381
mateuszd2411 <mateuszd2411@gmail.com>
2021-09-23 21:31:30 +0200

    Set moving for the player andd added .gitignore file

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed;
+    public float jumpForce;
+
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode jump;
+    public KeyCode throwBall;
+
+    private Rigidbody2D theRB;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        theRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(left))
+        {
+            theRB.velocity = new Vector2(-moveSpeed, theRB.velocity.y);
+        }else if(Input.GetKey(right))
+        {
+            theRB.velocity = new Vector2(moveSpeed, theRB.velocity.y);
+        }else
+        {
+            theRB.velocity = new Vector2(0, theRB.velocity.y);
+        }
+
+        if(Input.GetKeyDown(jump))
+        {
+            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+        }
     }
 }
