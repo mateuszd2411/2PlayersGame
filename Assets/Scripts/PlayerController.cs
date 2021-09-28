885446c158a9de176715be643c8ee85f82de4010
mateuszqaz2411 <mateuszqaz2411@gmail.com>
2021-09-28 19:17:45 +0200

    Added animations for player 1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,15 +14,28 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D theRB;
 
+    public Transform groundCheckPoint;
+    public float groundCheckRadious;
+    public LayerMask whatIsGround;
+
+    public bool isGrounded;
+
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
         theRB = GetComponent<Rigidbody2D>();
+
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadious, whatIsGround);
+
         if(Input.GetKey(left))
         {
             theRB.velocity = new Vector2(-moveSpeed, theRB.velocity.y);
@@ -34,9 +47,20 @@ public class PlayerController : MonoBehaviour
             theRB.velocity = new Vector2(0, theRB.velocity.y);
         }
 
-        if(Input.GetKeyDown(jump))
+        if(Input.GetKeyDown(jump) && isGrounded)
         {
             theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
         }
+
+        if(theRB.velocity.x <0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }else if (theRB.velocity.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        anim.SetFloat("Speed", Mathf.Abs(theRB.velocity.x));
+        anim.SetBool("Grounded", isGrounded);
     }
 }
